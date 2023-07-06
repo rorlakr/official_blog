4e65bf6da3313064c2358258d8051f65277fd7a6
Lucius Choi <lucius.choi@gmail.com>
2023-07-06 18:40:39 +0900

    파일 정리

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  // base: "/official_blog",
   integrations: [
     tailwind({
       config: {
